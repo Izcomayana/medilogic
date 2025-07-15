91cd5cc490ac8d906712214cda584b0d76dafe35
Izcomayana <oluwapelumisotoyinbo@gmail.com>
2025-07-15 20:54:39 +0100

    features page: hero and super admin sections

--- app/(landingpage)/features/index.tsx
+++ app/(landingpage)/features/index.tsx
@@ -1,5 +1,25 @@
+"use client";
 import React from "react";
+import Hero from "./components/Hero";
+import SuperAdmin from "./components/SuperAdmin";
 
 export const Features = () => {
-  return <div>Features</div>;
+  return (
+    <div className="min-h-screen bg-gradient-to-br from-gray-50 to-white overflow-x-hidden">
+      <Hero />
+      <SuperAdmin />
+
+      {/* <ClinicAdminSection /> */}
+
+      {/* <DriverSection /> */}
+
+      {/* <ClientSection /> */}
+
+      {/* <AIAnalyticsSection /> */}
+
+      {/* <SecuritySection /> */}
+
+      {/* <FeaturesCTASection /> */}
+    </div>
+  );
 };
