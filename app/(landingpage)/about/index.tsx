a0d51f80b9e1405c1146ee1975754520e6b26f8b
Izcomayana <oluwapelumisotoyinbo@gmail.com>
2025-07-15 15:08:49 +0100

    about page: whatmakesusdiff and whoweserve sections

--- app/(landingpage)/about/index.tsx
+++ app/(landingpage)/about/index.tsx
@@ -1,21 +1,17 @@
 import React from "react";
 import Hero from "./components/Hero";
 import WhyMedilogic from "./components/WhyMedilogic";
+import WhatMakesUsDiff from "./components/WhatMakesUsDiff";
+import WhoWeServe from "./components/WhoWeServe";
 
 export const About = () => {
   return (
     <div className="min-h-screen bg-gradient-to-br from-gray-50 to-white overflow-x-hidden">
       <Hero />
       <WhyMedilogic />
-
-      {/* What Makes Medilogic Different */}
-      {/* <WhatMakesDifferentSection /> */}
-
-      {/* Who We Serve */}
-      {/* <WhoWeServeSection /> */}
-
-      {/* Our Vision */}
-      {/* <VisionSection /> */}
+      <WhatMakesUsDiff />
+      <WhoWeServe />
+      {/* <Vision /> */}
 
       {/* Compliance by Design */}
       {/* <ComplianceSection /> */}
