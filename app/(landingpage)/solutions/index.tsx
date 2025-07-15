b1830c459dc2df0154df6fe8435335fdc7e5f06c
Izcomayana <oluwapelumisotoyinbo@gmail.com>
2025-07-15 23:41:57 +0100

    solutions page: PrivatePractice and WasteDisposal

--- app/(landingpage)/solutions/index.tsx
+++ app/(landingpage)/solutions/index.tsx
@@ -1,15 +1,16 @@
 import React from "react";
 import Hero from "./components/Hero";
 import NHSSolution from "./components/NHSSolutions";
+import PrivatePractice from "./components/PrivatePractice";
+import WasteDisposal from "./components/WasteDisposal";
 
 export const Solutions = () => {
   return (
     <div className="min-h-screen bg-gradient-to-br from-gray-50 to-white overflow-x-hidden">
       <Hero />
       <NHSSolution />
-
-      {/* <PrivatePractice /> */}
-      {/* <WasteDisposal /> */}
+      <PrivatePractice />
+      <WasteDisposal />
 
       {/* Medical Couriers & Logistics Providers */}
       {/* <CourierSection /> */}
