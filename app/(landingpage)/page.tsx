1d5c5a3fba81f2eb109856905aa5bfa86055d613
Izcomayana <oluwapelumisotoyinbo@gmail.com>
2025-07-17 14:52:32 +0100

    hero section made dynamic for reusability purposes

--- app/(landingpage)/page.tsx
+++ app/(landingpage)/page.tsx
@@ -2,18 +2,35 @@
 
 import { PlatformFeatures } from "./components/PlatformFeatures";
 import AIAnalytics from "@/components/AIAnalytics";
-import { Hero } from "./components/Hero";
+import { Hero } from "@/components/Hero";
 import { BFHP } from "./components/BFHP";
 import { Security } from "@/components/Security";
 import { QuickStart } from "./components/QuickStart";
 import { CTA } from "@/components/CTA";
 import { KeyFeatures } from "./components/KeyFeatures";
-import { Phone } from "lucide-react";
+import { ArrowRight, Award, Phone, Play, Shield } from "lucide-react";
 
 export default function HomePage() {
   return (
     <div className="min-h-screen bg-gradient-to-br from-gray-50 to-white overflow-x-hidden">
-      <Hero />
+      <Hero
+        heading="Transforming Clinical Logistics with"
+        heading2="AI-Powered Precision"
+        subText="A secure, intelligent platform for managing clinical waste transport,
+          medical deliveries, and NHS-compliant reporting."
+        descBadgeIcon={<Shield className="w-4 h-4 mr-2" />}
+        descBadgeText="GDPR & ISO 27001 Ready"
+        descBadgeIcon2={<Award className="w-4 h-4 mr-2" />}
+        descBadgeText2="Built for NHS Clinics, Private Practices & Medical Couriers"
+        CTABtn="Get a Demo"
+        CTABtnIcon={
+          <ArrowRight className="ml-2 w-5 h-5 group-hover:translate-x-1 transition-transform" />
+        }
+        CTABtn2Icon={
+          <Play className="mr-2 w-5 h-5 group-hover:scale-110 transition-transform" />
+        }
+        CTABtn2="Learn How It Works"
+      />
       <BFHP />
       <KeyFeatures />
       <PlatformFeatures />
