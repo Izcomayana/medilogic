d85ad5192bee141bb1fc9e1a2eb664e646b74385
Izcomayana <oluwapelumisotoyinbo@gmail.com>
2025-08-07 13:59:32 +0100

    fetxhed the no of organizatons

--- app/(dashboard)/layout.tsx
+++ app/(dashboard)/layout.tsx
@@ -1,11 +1,8 @@
 import type React from "react";
-import { Inter } from "next/font/google";
 import "./globals.css";
 import { SidebarProvider } from "@/components/ui/sidebar";
 import { AppSidebar } from "@/components/ui/app-sidebar";
 import { AuthProvider } from "@/components/auth";
-
-const inter = Inter({ subsets: ["latin"] });
 
 export default function RootLayout({
   children,
@@ -13,17 +10,13 @@ export default function RootLayout({
   children: React.ReactNode;
 }) {
   return (
-    <html lang="en" suppressHydrationWarning>
-      <body className={inter.className} suppressHydrationWarning>
-        <AuthProvider>
-          <SidebarProvider>
-            <AppSidebar />
-            <main className="flex-1 bg-gray-900 min-h-screen max-w-dvw">
-              {children}
-            </main>
-          </SidebarProvider>
-        </AuthProvider>
-      </body>
-    </html>
+    <AuthProvider>
+      <SidebarProvider>
+        <AppSidebar />
+        <main className="flex-1 bg-gray-900 min-h-screen max-w-dvw">
+          {children}
+        </main>
+      </SidebarProvider>
+    </AuthProvider>
   );
 }
