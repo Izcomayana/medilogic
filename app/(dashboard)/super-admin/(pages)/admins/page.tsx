54f1d25e40fef1ddd352e6fb0c72a37dde04965b
Victor Olusanmi-Sogbein <olusanmiv@gmail.com>
2025-09-13 18:30:43 +0100

    Formatted

--- app/(dashboard)/super-admin/(pages)/admins/page.tsx
+++ app/(dashboard)/super-admin/(pages)/admins/page.tsx
@@ -125,7 +125,9 @@ export default function OrgAdmin() {
                   <Building2 className="h-4 w-4 mr-2" />
                   <SelectValue
                     placeholder={
-                      loading ? 'Loading organizations...' : 'Select organization'
+                      loading
+                        ? 'Loading organizations...'
+                        : 'Select organization'
                     }
                   />
                 </SelectTrigger>
